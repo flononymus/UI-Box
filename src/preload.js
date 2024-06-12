6fa347a1e97d8216420209da8800f407d6ee9f40
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-06-12 18:42:58 +0200

    dark mode not working

--- src/preload.js
+++ src/preload.js
@@ -9,10 +9,17 @@
 //     }
 //   })
 
-const { contextBridge } = require('electron/renderer')
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
+// contextBridge.exposeInMainWorld('versions', {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron
+// })
+
+
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
 })
